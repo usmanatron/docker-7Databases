d7d327e64f8ca7f7045bab22c250ca936f041bee
Usman Iqbal <usman.iqbal@softwire.com>
2019-06-24 12:17:47 +0100

    Clear volumes from HBase when tearing down - stops missing regions popping up on UI

--- 02_hbase/local_down.ps1
+++ 02_hbase/local_down.ps1
@@ -1,1 +1,7 @@
 docker-compose -f docker-hbase\docker-compose-standalone.yml down
+
+docker volume rm docker-hbase_hadoop_namenode
+docker volume rm docker-hbase_hadoop_datanode
+docker volume rm docker-hbase_hadoop_historyserver
+docker volume rm docker-hbase_hbase_data
+docker volume rm docker-hbase_hbase_zookeeper_data
