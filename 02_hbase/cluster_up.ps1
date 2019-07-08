5871df406c8bb35f2dcaef1b72eb0ddf7c25cb35
Usman Iqbal <usman.iqbal@softwire.com>
2019-07-08 14:40:45 +0100

    Open bash when using cluster_up so you can call scripts

--- 02_hbase/cluster_up.ps1
+++ 02_hbase/cluster_up.ps1
@@ -5,4 +5,6 @@ docker-compose -f docker-images\docker-compose-distributed-local.yml up --detach
 [Diagnostics.Process]::Start('http://localhost:16010')
 
 # Connect to HBase shell
-docker exec -it hbase-master /opt/hbase-1.2.6/bin/hbase shell
+Write-Host -ForegroundColor Yellow "To connect to the shell: /opt/hbase-1.2.6/bin/hbase shell"
+Write-Host -ForegroundColor Yellow "To access scripts: /tmp/scripts/"
+docker exec -it hbase-master /bin/bash
