3bebad938cfb6b9b6a66b156dd72a7965de22383
Usman Iqbal <usman.iqbal@softwire.com>
2019-07-22 14:17:01 +0100

    Finish mongo local

--- 03_mongodb/local_up.ps1
+++ 03_mongodb/local_up.ps1
@@ -1,9 +1,10 @@
 Write-Host -ForegroundColor Yellow "Starting MongoDB..."
 docker-compose up --detach
 
-Write-Host -ForegroundColor Yellow "Opening mongo-express (for visual representation)"
+Write-Host -ForegroundColor Yellow "Opening GUI (mongo-express)"
 [Diagnostics.Process]::Start('http://localhost:8083')
 
 Write-Host -ForegroundColor Yellow "Connecting to the CLI..."
 Start-Sleep -Seconds 3
+Write-Host -ForegroundColor Yellow "Username: mongo; Password: gonmo"
 docker exec -it 7dbs_mongo_db mongo
