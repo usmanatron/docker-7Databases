ccd407e5ea00b0418f6f77acdd81b6f7a2bcf53c
Usman Iqbal <usman.iqbal@softwire.com>
2019-07-22 14:37:31 +0100

    Tidy up clustering scripts to include GUI \ connect to mongos (sic) properly

--- 03_mongodb/cluster_down.ps1
+++ 03_mongodb/cluster_down.ps1
@@ -1,1 +1,1 @@
-docker-compose -f cluster\mongo-cluster-docker\docker-compose.1.yml -f cluster\mongo-cluster-docker\docker-compose.2.yml  -f cluster\mongo-cluster-docker\docker-compose.cnf.yml -f cluster\mongo-cluster-docker\docker-compose.shard.yml down
+docker-compose -f cluster\mongo-cluster-docker\docker-compose.1.yml -f cluster\mongo-cluster-docker\docker-compose.2.yml  -f cluster\mongo-cluster-docker\docker-compose.cnf.yml -f cluster\mongo-cluster-docker\docker-compose.shard.yml -f cluster\docker-compose.gui.yml down
--- 03_mongodb/cluster_up.ps1
+++ 03_mongodb/cluster_up.ps1
@@ -1,1 +1,6 @@
-docker-compose -f cluster\mongo-cluster-docker\docker-compose.1.yml -f cluster\mongo-cluster-docker\docker-compose.2.yml  -f cluster\mongo-cluster-docker\docker-compose.cnf.yml -f cluster\mongo-cluster-docker\docker-compose.shard.yml up
+docker-compose -f cluster\mongo-cluster-docker\docker-compose.1.yml -f cluster\mongo-cluster-docker\docker-compose.2.yml  -f cluster\mongo-cluster-docker\docker-compose.cnf.yml -f cluster\mongo-cluster-docker\docker-compose.shard.yml -f cluster\docker-compose.gui.yml up --detach
+
+# No authentication needed
+Write-Host -ForegroundColor Yellow "Connecting to the CLI..."
+Start-Sleep -Seconds 3
+docker exec -it mongo-router mongo
