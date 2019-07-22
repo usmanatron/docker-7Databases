e101901ab96da3f651e722c9debde49fdc6e5039
Usman Iqbal <usman.iqbal@softwire.com>
2019-07-22 14:17:22 +0100

    Add clustering version of mongo

---
+++ 03_mongodb/cluster_up.ps1
@@ -0,0 +1,1 @@
+docker-compose -f cluster\mongo-cluster-docker\docker-compose.1.yml -f cluster\mongo-cluster-docker\docker-compose.2.yml  -f cluster\mongo-cluster-docker\docker-compose.cnf.yml -f cluster\mongo-cluster-docker\docker-compose.shard.yml up
